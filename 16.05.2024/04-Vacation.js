432f0898c3576ac4b8b9b9de9ec2cc98c681f38d
madvin <madvin@abv.bg>
2024-05-18 14:47:08 +0300

    aaa

--- 16.05.2024/04-Vacation.js
+++ 16.05.2024/04-Vacation.js
@@ -5,54 +5,49 @@ function vacation(group, type, day) {
     if (day === 'Friday') {
         switch (type) {
             case 'Students':
-                price += group * 8.45;
+                price = 8.45;
                 break;
             case 'Business':
-                price += group * 10.9;
+                price = 10.9;
                 break;
             case 'Regular':
-                price += group * 8.45;
+                price = 15;
                 break;
         }
     } else if (day === 'Saturday') {
         switch (type) {
             case 'Students':
-                price += group * 9.8;
+                price = 9.8;
                 break;
             case 'Business':
-                price += group * 15.6;
+                price = 15.6;
                 break;
             case 'Regular':
-                price += group * 20;
+                price = 20;
                 break;
         }
     } else if (day === 'Sunday') {
         switch (type) {
             case 'Students':
-                price += group * 10.46;
+                price = 10.46;
                 break;
             case 'Business':
-                price += group * 16;
+                price = 16;
                 break;
             case 'Regular':
-                price += group * 22.5;
+                price = 22.5;
                 break;
         }
     }
+    totalPrice = price * group
     if (type === 'Students' && group >= 30) {
-        totalPrice += price * 0.85;
+        totalPrice = totalPrice * 0.85;
     } else if (type === 'Business' && group >= 100) {
-        totalPrice += price - price * 10;
+        totalPrice = totalPrice - price * 10;
     } else if (type === 'Regular' && group >= 10 && group <= 20) {
-        totalPrice += price * 0.95;
-    } else {
-        totalPrice = price;
+        totalPrice = totalPrice * 0.95;
     }
     console.log(`Total price: ${totalPrice.toFixed(2)}`)
 }
 
-vacation([ 30,
-
-"Students",
-
-"Sunday"])
+vacation(30, "Students", "Sunday")
