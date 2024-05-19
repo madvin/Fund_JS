5e71b3d23a19fcaf80802cc6e81785891953992a
madvin <madvin@abv.bg>
2024-05-20 02:13:29 +0300

    last one

--- 16.05.2024/05-LeapYear.js
+++ 16.05.2024/05-LeapYear.js
@@ -1,9 +1,9 @@
 function leapYear(year) {
-    if (year % 4 === 0 || year % 100 === 1 || year % 400 === 0 ) {
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0 ) {
         console.log('yes');
     } else {
         console.log('no')
     }
 }
 
-leapYear(2016)
+leapYear(1903)
