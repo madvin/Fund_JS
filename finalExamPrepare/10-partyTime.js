41fde971ec4b883eb2af97e3c376f3962a56c7e4
madvin <madvin@abv.bg>
2024-08-04 01:20:53 +0300

    lastone

--- finalExamPrepare/10-partyTime.js
+++ finalExamPrepare/10-partyTime.js
@@ -1,86 +1,73 @@
 function solve(input) {
-  let vip = [];
-  let people = [];
-  let guestCount = 0;
-  let pattern = /[0-9]/;
-  let totalGuestList = [];
-
+  const totalGuestList = new Set();
   let guest = input.shift();
 
   while (guest !== "PARTY") {
-    
-    totalGuestList.push(guest)
+    totalGuestList.add(guest);
     guest = input.shift();
   }
 
-  for (const unit of totalGuestList) {
-
-        for (let guest of input) {
-            if (unit === guest) {
-                guestCount++;
-           if (guest.firstCharAt(0) === pattern) {
-            console.log('-------------');
-            
-           }
-        }
-    
-    
-    
+  for (const guestInList of totalGuestList) {
+    for (const guestArrive of input) {
+      if (guestInList === guestArrive) {
+        totalGuestList.delete(guestArrive);
+      }
+    }
   }
-let totalGuestCame = totalGuestList.length - guestCount
-console.log(totalGuestCame);
+  let arrToSort = Array.from(totalGuestList);
+  let sortedList = arrToSort.sort((a, b) => a - b);
 
-
-  
+  console.log(totalGuestList.size);
+  console.log(sortedList.join("\n"));
 }
 
-solve([
-  "7IK9Yo0h",
-  "9NoBUajQ",
-  "Ce8vwPmE",
-  "SVQXQCbc",
-  "tSzE5t0p",
-  "PARTY",
-  "9NoBUajQ",
-  "Ce8vwPmE",
-  "SVQXQCbc",
-]);
+// solve([
+//   "7IK9Yo0h",
+//   "9NoBUajQ",
+//   "Ce8vwPmE",
+//   "SVQXQCbc",
+//   "tSzE5t0p",
+//   "PARTY",
+//   "9NoBUajQ",
+//   "Ce8vwPmE",
+//   "SVQXQCbc",
+// ]);
 
 //Output:
 // 2
 // 7IK9Yo0h
 // tSzE5t0p
 
-// solve(['m8rfQBvl',
-//     'fc1oZCE0',
-//     'UgffRkOn',
-//     '7ugX7bm0',
-//     '9CQBGUeJ',
-//     '2FQZT3uC',
-//     'dziNz78I',
-//     'mdSGyQCJ',
-//     'LjcVpmDL',
-//     'fPXNHpm1',
-//     'HTTbwRmM',
-//     'B5yTkMQi',
-//     '8N0FThqG',
-//     'xys2FYzn',
-//     'MDzcM9ZK',
-//     'PARTY',
-//     '2FQZT3uC',
-//     'dziNz78I',
-//     'mdSGyQCJ',
-//     'LjcVpmDL',
-//     'fPXNHpm1',
-//     'HTTbwRmM',
-//     'B5yTkMQi',
-//     '8N0FThqG',
-//     'm8rfQBvl',
-//     'fc1oZCE0',
-//     'UgffRkOn',
-//     '7ugX7bm0',
-//     '9CQBGUeJ'
-//     ])
+solve(['m8rfQBvl',
+    'fc1oZCE0',
+    'UgffRkOn',
+    '7ugX7bm0',
+    '9CQBGUeJ',
+    '2FQZT3uC',
+    'dziNz78I',
+    'mdSGyQCJ',
+    'LjcVpmDL',
+    'fPXNHpm1',
+    'HTTbwRmM',
+    'B5yTkMQi',
+    '8N0FThqG',
+    'xys2FYzn',
+    'MDzcM9ZK',
+    'PARTY',
+    '2FQZT3uC',
+    'dziNz78I',
+    'mdSGyQCJ',
+    'LjcVpmDL',
+    'fPXNHpm1',
+    'HTTbwRmM',
+    'B5yTkMQi',
+    '8N0FThqG',
+    'm8rfQBvl',
+    'fc1oZCE0',
+    'UgffRkOn',
+    '7ugX7bm0',
+    '9CQBGUeJ'
+    ])
 
 //Output:
 // 2
